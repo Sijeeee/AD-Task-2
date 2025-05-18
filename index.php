7f552b5e5c889c6f6cc98c896b893f0bb5899e1f
ceej <ceejjay73@gmail.com>
2025-05-18 12:33:50 +0800

    feat (index.php) added dictionary

--- index.php
+++ index.php
@@ -19,6 +19,57 @@
         7 => ["Nissan", "GTR Nismo", 2020],
         8 => ["Mazda", "RX-7 FD", 2010]
     ];
+
+    $cardict = [
+        1 => [
+            "make" => "Toyota",
+            "model" => "Camry",
+            "year" => "2020",
+            "type" => "Sedan"
+        ],
+        2 => [
+            "make" => "Honda",
+            "model" => "Civic",
+            "year" => "2021",
+            "type" => "Sedan"
+        ],
+        3 => [
+            "make" => "Ford",
+            "model" => "Focus",
+            "year" => "2019",
+            "type" => "Hatchback"
+        ],
+        4 => [
+            "make" => "Honda",
+            "model" => "NSX",
+            "year" => "2000",
+            "type" => "Sports"
+        ],
+        5 => [
+            "make" => "Toyota",
+            "model" => "Supra",
+            "year" => "2020",
+            "type" => "Super"
+        ],
+        6 => [
+            "make" => "Mclaren",
+            "model" => "F1",
+            "year" => "2000",
+            "type" => "Sports"
+        ],
+        7 => [
+            "make" => "Nissan",
+            "model" => "GTR Nismo",
+            "year" => "2020",
+            "type" => "Sports"
+        ],
+        8 => [
+            "make" => "Mazda",
+            "model" => "RX-7 FD",
+            "year" => "2010",
+            "type" => "Sports"
+        ],
+    ]
     
     foreach ($cars as $id => $car) {
         $url = "page/cardetails.php?id=$id";
